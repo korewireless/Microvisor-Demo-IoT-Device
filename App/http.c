f7cf8a477495a5e036f4b283ef2bb42d26663143
Tony Smith <tsmith@twilio.com>
2023-01-26 14:34:52 +0000

    More useful log output

--- App/http.c
+++ App/http.c
@@ -90,7 +90,7 @@ void http_close_channel(void) {
         MvChannelHandle old = http_handles.channel;
         enum MvStatus status = mvCloseChannel(&http_handles.channel);
         if (status != MV_STATUS_OKAY && status != MV_STATUS_CHANNELCLOSED) report_and_assert(ERR_CHANNEL_NOT_CLOSED);
-        server_log("HTTP channel %lu closed", (uint32_t)old);
+        server_log("HTTP channel %lu closed (status code: %i)", (uint32_t)old, status);
     }
 
     // Confirm the channel handle has been invalidated by Microvisor
